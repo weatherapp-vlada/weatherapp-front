ca73a6f559d4b4174bc7e3e8da385d78815a42b3
Vladimir Dabic <vladadaba92@gmail.com>
2023-03-02 10:29:11 +0100

    fun with algebraic types

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-TODO
+## TODO
+
+- [x] Api client generation using `openapi-generator-cli`
+- [x] Algebraic types using `@swan-io/boxed`
+- [ ] `D3.js` vizualization
+- [ ] Add `react-query`
+- [ ] AWS Amplify login and signup
+- [ ] Styling
+- [ ] Refactor into separate components
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import Head from "next/head";
 import { useEffect, useState } from "react";
 import { utc } from "moment";
-import styles from "@/styles/Home.module.css";
+import { match, P } from "ts-pattern";
+import { AsyncData, Option, Result } from "@swan-io/boxed";
 
+import styles from "@/styles/Home.module.css";
 import {
   Configuration,
   LocationApiFactory,
@@ -10,6 +12,13 @@ import {
   WeatherApiFactory,
   WeatherDto,
 } from "@/api-client";
+
+const { NotAsked, Loading, Done } = AsyncData.pattern;
+const { Ok, Error } = Result.pattern;
+const { Some, None } = Option.pattern;
+
+type LocationsState = AsyncData<Result<Option<LocationDto[]>, number>>;
+type WeatherDataState = AsyncData<Result<Option<WeatherDto[]>, number>>;
 
 const locationApi = LocationApiFactory(
   new Configuration({
@@ -24,33 +33,46 @@ const weatherApi = WeatherApiFactory(
 );
 
 export default function Home() {
-  const [locations, setLocations] = useState<LocationDto[]>([]);
-  const [selectedLocation, setSelectedLocation] = useState<number>(-1);
-  const [weatherData, setWeatherData] = useState<WeatherDto[]>();
+  const [locations, setLocations] = useState<LocationsState>(
+    AsyncData.NotAsked()
+  );
+  const [selectedLocation, setSelectedLocation] = useState<
+    number | undefined
+  >();
+  const [weatherData, setWeatherData] = useState<WeatherDataState>(
+    AsyncData.NotAsked()
+  );
 
   useEffect(() => {
-    (async () => {
-      const {
-        data: { locations: locationResponse },
-      } = await locationApi.locationControllerGetLocations();
-      setLocations(locationResponse);
-    })();
+    setLocations(AsyncData.Loading());
+    locationApi
+      .locationControllerGetLocations()
+      .then(({ data: { locations: locationsResponse } }) =>
+        setLocations(
+          AsyncData.Done(Result.Ok(Option.fromNull(locationsResponse)))
+        )
+      )
+      .catch((err) => setLocations(AsyncData.Done(Result.Error(err))));
   }, []);
 
   useEffect(() => {
-    if (selectedLocation !== -1) {
-      (async () => {
-        const now = utc();
-        const {
-          data: { locations: locationsResponse },
-        } = await weatherApi.forecastControllerGetWeather({
+    if (selectedLocation !== undefined) {
+      setWeatherData(AsyncData.Loading());
+      const now = utc();
+      weatherApi
+        .forecastControllerGetWeather({
           startDate: now.format(),
           endDate: now.add(1, "d").format(),
           locationIds: [selectedLocation],
-        });
-
-        setWeatherData(locationsResponse[0].weather);
-      })();
+        })
+        .then(({ data: { locations: locationsResponse } }) =>
+          setWeatherData(
+            AsyncData.Done(
+              Result.Ok(Option.fromNull(locationsResponse[0].weather))
+            )
+          )
+        )
+        .catch((err) => setWeatherData(AsyncData.Done(Result.Error(err))));
     }
   }, [selectedLocation]);
 
@@ -60,44 +82,66 @@ export default function Home() {
         <title>Weather App</title>
       </Head>
       <main className={styles.main}>
-        <h3>Select city:</h3>
-        <select
-          value={selectedLocation}
-          onChange={(event: React.ChangeEvent<HTMLSelectElement>) => {
-            setSelectedLocation(Number(event.target.value));
-          }}
-        >
-          {locations.map((location) => (
-            <option key={location.id} value={location.id}>
-              {location.name}, {location.countryCode}
-            </option>
-          ))}
-        </select>
-        {weatherData && (
-          <div>
-            <h3>Selected location weather</h3>
-            {weatherData.map(
-              ({
-                timestamp,
-                isNight,
-                temperatureCelsius,
-                humidity,
-                precipitationProbability,
-                pressure,
-                weatherDescription,
-                windDirection,
-                windSpeedMetersPerSecond,
-                rainVolumePast3HoursMm,
-                snowVolumePast3HoursMm,
-              }) => (
-                <div key={timestamp}>
-                  <span>timestamp: {timestamp}</span>
-                  <span>temperatureCelsius: {temperatureCelsius}</span>
-                </div>
-              )
-            )}
-          </div>
-        )}
+        {match(locations)
+          .with(NotAsked, Loading, () => null)
+          .with(
+            Done(Error(P.any)),
+            () => "An error occurred while retrieving supported locations"
+          )
+          .with(Done(Ok(None)), () => "No locations was received")
+          .with(Done(Ok(Some(P.select()))), (locations) => (
+            <div>
+              <h3>Select city:</h3>
+              <select
+                value={selectedLocation}
+                onChange={(event: React.ChangeEvent<HTMLSelectElement>) => {
+                  setSelectedLocation(Number(event.target.value));
+                }}
+              >
+                {locations.map((location) => (
+                  <option key={location.id} value={location.id}>
+                    {location.name}, {location.countryCode}
+                  </option>
+                ))}
+              </select>
+            </div>
+          ))
+          .exhaustive()}
+
+        {match(weatherData)
+          .with(NotAsked, Loading, () => null)
+          .with(
+            Done(Error(P.any)),
+            () =>
+              "An error occurred while retrieving weather data for selected location"
+          )
+          .with(Done(Ok(None)), () => "No weather data was received")
+          .with(Done(Ok(Some(P.select()))), (weatherData) => (
+            <div>
+              <h3>Selected location weather</h3>
+              {weatherData.map(
+                ({
+                  timestamp,
+                  isNight,
+                  temperatureCelsius,
+                  humidity,
+                  precipitationProbability,
+                  pressure,
+                  weatherDescription,
+                  windDirection,
+                  windSpeedMetersPerSecond,
+                  rainVolumePast3HoursMm,
+                  snowVolumePast3HoursMm,
+                }) => (
+                  <div key={timestamp}>
+                    <span>timestamp: {timestamp}</span>
+                    <span>temperatureCelsius: {temperatureCelsius}</span>
+                  </div>
+                )
+              )}
+            </div>
+          ))
+          .exhaustive()}
       </main>
     </>
   );
